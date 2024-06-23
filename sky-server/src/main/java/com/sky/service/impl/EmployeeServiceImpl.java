7b6e4ab1d8df304e362a76ec8f971af99f36ee37
gosling-fan <dzp183ban@gmail.com>
2024-06-23 19:20:01 +0800

    苍穹外卖项目新增员工部分

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,10 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +12,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -39,7 +45,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        //对前端传过来的明文密码进行md5加密操作
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -52,6 +59,41 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     * 员工新增
+     * @param employeeDTO
+     */
+    @Override
+    public void add(EmployeeDTO employeeDTO) {
+        //1.接受来自前端的员工属性
+        Employee employee = new Employee();
+        //借助spring工具类，拷贝员工属性
+        BeanUtils.copyProperties(employeeDTO,employee);
+        //2.添加剩余员工表需设置的信息
+        //设置员工账号状态0代表锁定，1代表正常，默认正常
+        employee.setStatus(StatusConstant.ENABLE);
+        //用MD5加密前端的明文密码
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()) );
+        //设置创建和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+        //设置操作的用户id
+        employee.setUpdateUser(BaseContext.getCurrentId());
+        employee.setCreateUser(BaseContext.getCurrentId());
+
+        employeeMapper.add(employee);
+    }
+
+    /**
+     * 根据用户名查询用户
+     * @param username
+     */
+    @Override
+    public Employee selectEmployeeByusername(String username) {
+        Employee employeeMapperByUsername = employeeMapper.getByUsername(username);
+        return employeeMapperByUsername;
     }
 
 }
