c8653ed01dea95075f45e6cc67a58a1336b9b639
gosling-fan <dzp183ban@gmail.com>
2024-06-27 17:02:25 +0800

    店铺营业管理

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,30 @@
+package com.sky.controller.user;
+
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("UserShopController")
+@Slf4j
+@RequestMapping("/user/shop")
+@Api(tags = "店铺管理")
+public class ShopController {
+    public static final String KEY = "SH0P_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+    /**
+     * 获取店铺营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺营业状态")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        return Result.success(status);
+    }
+}
